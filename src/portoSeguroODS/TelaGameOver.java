07aa8b95a90e228dc12f4e82884e2e3ed49ac83a
gersonespejo127@gmail.com <gersonespejo127@gmail.com>
2022-03-13 20:28:59 -0300

    tela de game over atualizado

--- src/portoSeguroODS/TelaGameOver.java
+++ src/portoSeguroODS/TelaGameOver.java
@@ -12,6 +12,9 @@ import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TelaGameOver extends JFrame {
 
@@ -49,6 +52,16 @@ public class TelaGameOver extends JFrame {
 		contentPane.setLayout(null);
 		
 		QuizMatematica.setPontuacao(9);
+		
+		JButton botaoSair = new JButton("Sair");
+		botaoSair.setBackground(Color.LIGHT_GRAY);
+		botaoSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		botaoSair.setBounds(195, 227, 89, 23);
+		contentPane.add(botaoSair);
 		JLabel pontos = new JLabel("Pontos: "+QuizMatematica.getPontuacao());
 		pontos.setFont(new Font("Arial Black", Font.PLAIN, 16));
 		pontos.setForeground(Color.WHITE);
@@ -56,7 +69,7 @@ public class TelaGameOver extends JFrame {
 		contentPane.add(pontos);
 		
 		JLabel fundoGameOver = new JLabel();
-		fundoGameOver.setBounds(0, 0, 480, 270);
+		fundoGameOver.setBounds(0, -10, 480, 270);
 		fundoGameOver.setIcon(new ImageIcon("C:\\workspace-eclipse\\workspace\\ProejtoQuiz\\img\\gameOver.gif"));
 		contentPane.add(fundoGameOver);
 		FundoGameOver.setLayout(new BorderLayout(0, 0));
