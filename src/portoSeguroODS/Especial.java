d931e6f878026635aeeec8cd8b3a8026c5f2e581
KevimLSDS <kevim7@hotmail.com>
2022-03-15 16:55:38 -0300

    Refatoração da Especial para nova Strategy

--- src/portoSeguroODS/Especial.java
+++ src/portoSeguroODS/Especial.java
@@ -1,6 +1,6 @@
 package portoSeguroODS;
 
-public class Especial extends QuizMatematica implements Perguntas {
+public class Especial extends QuizMatematica implements PerguntaQuiz, Calculo {
 
 	public Especial() {
 		super();
@@ -12,32 +12,56 @@ public class Especial extends QuizMatematica implements Perguntas {
 
 	@Override
 	public void fazerQuiz() {
-		System.out.println("\nVidas: " + getVida());
-		
+		while (QuizMatematica.getPontuacao() == 9 && QuizMatematica.getVida().size() > 0) {
+
+			System.out.println("\nVidas: " + getVida());
+
+			calcular();
+
+			if (perdeu()) {
+				break;
+			}
+		}
+	}
+
+	@Override
+	public void calcular() {
+
 		if (getA() > getB()) {
 			System.out.println("Qual é o resultado desta operação: ");
 			// a² + (a - b) x (c + d) + √4
-			System.out.println(getA() + "² + (" + getA() + " - " + getB() + ") x (" + getC() + "+" + getD() + ") + √4?");
+			System.out.println(getA() + "² + (" 
+					+ getA() + " - " + getB() + ") x (" 
+					+ getC() + "+" + getD() + ") + √4?");
+
 			setRespostaCorreta((int) Math.pow(getA(), 2) + (getA() - getB()) * (getC() + getD()) + (int) Math.sqrt(4));
+
+			checarResposta();
 		} else {
 			System.out.println("Qual é o resultado desta operação: ");
 			// a² + (b - a) x (c + d) + √9
-			System.out.println(getA() + "² + (" + getB() + " - " + getA() + ") x (" + getC() + "+" + getD() + ") + √9?");
-			setRespostaCorreta((int) Math.pow(getA(), 2) + (getB() - getA()) * (getC() + getD()) + (int) Math.sqrt(9));
-		}
+			System.out.println(getA() + "² + (" 
+					+ getB() + " - " + getA() + ") x (" 
+					+ getC() + "+" + getD() + ") + √9?");
 
+			setRespostaCorreta((int) Math.pow(getA(), 2) + (getB() - getA()) * (getC() + getD()) + (int) Math.sqrt(9));
+
+			checarResposta();
+		}
 	}
 
-	/*
-	 * @Override public void checaResposta() { setRespostaDoUsuario(0); if
-	 * (getRespostaCorreta() == getRespostaDoUsuario()) {
-	 * System.out.println("\nReposta correta!"); setPontuacao(1);
-	 * QuizMatematica.parabens(); } else {
-	 * System.out.println("\nResposta incorreta!" + "\nA resposta é: " +
-	 * getRespostaCorreta()); System.out.println("\nVocê perdeu uma vida!\n");
-	 * removeVidas(); }
-	 * 
-	 * }
-	 */
+	@Override
+	public void checarResposta() {
+		setRespostaDoUsuario(0); // Scanner do Usuário
 
+		if (getRespostaCorreta() == getRespostaDoUsuario()) {
+			System.out.println("\nReposta correta!");
+			setPontuacao(1);
+			mostreAPontuacao();
+		} else {
+			System.out.println("\nResposta incorreta!" + "\nA resposta é: " + getRespostaCorreta());
+			System.out.println("\nVocê perdeu uma vida!\n");
+			removeVidas();
+		}
+	}
 }
